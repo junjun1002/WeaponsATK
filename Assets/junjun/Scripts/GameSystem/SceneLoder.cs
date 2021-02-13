ec3ee6e79e9aca88c6beb7dacb85b2a4ef1876eb
junjun1002 <ueno.junya@outlook.jp>
2021-02-14 05:13:18 +0900

    シーンのロード実装？ 今度VRでテストしよ

---
+++ Assets/junjun/Scripts/GameSystem/SceneLoder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoder : SingletonMonoBehavior<SceneLoder>
+{
+    //[SerializeField] OVRScreenFade m_screenFade;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        //m_screenFade = gameObject.GetComponent<OVRScreenFade>();
+    }
+    public void LodeTitle()
+    {
+        //m_screenFade.FadeIn();
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+}
