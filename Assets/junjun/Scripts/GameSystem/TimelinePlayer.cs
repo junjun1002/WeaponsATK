7c2718a865831eec1a847bad0a9894cd7750c747
junjun1002 <ueno.junya@outlook.jp>
2021-02-14 04:01:28 +0900

    ゲームシステムにTimeLineを扱うためTimeLinePlayer実装

---
+++ Assets/junjun/Scripts/GameSystem/TimelinePlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelinePlayer : SingletonMonoBehavior<TimelinePlayer>
+{
+    public PlayableDirector playableDirector;
+
+    void Start()
+    {
+        //同じゲームオブジェクトにあるPlayableDirectorを取得する
+        playableDirector = GetComponent<PlayableDirector>();
+        PlayTimeline();
+    }
+
+    //再生する
+    void PlayTimeline()
+    {
+        playableDirector.Play();
+    }
+
+    //一時停止する
+    void PauseTimeline()
+    {
+        playableDirector.Pause();
+    }
+
+    //一時停止を再開する
+    void ResumeTimeline()
+    {
+        playableDirector.Resume();
+    }
+
+    //停止する
+    void StopTimeline()
+    {
+        playableDirector.Stop();
+    }
+
+}
