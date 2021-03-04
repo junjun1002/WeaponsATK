2673c26a5f2a6f7abe95e197f278c82158ec2de1
junjun1002 <ueno.junya@outlook.jp>
2021-03-05 03:43:41 +0900

    UIを設定中 とりあえずHPとSPはできた

---
+++ Assets/junjun/Scripts/Test/TestUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UIのテストをするクラス
+/// </summary>
+public class TestUI : MonoBehaviour
+{
+    [SerializeField] float m_useSP = 0.1f;
+
+        public void TestDamadeCause()
+    {
+        float DamageValue = Random.Range(0.02f, 0.1f);
+        UIManager.Instance.DecreasesHP(DamageValue);
+        Debug.Log(DamageValue * 100 + "ダメージを与えた");
+    }
+
+    public void TestUseSP()
+    {
+        UIManager.Instance.UseSP(m_useSP);
+        Debug.Log("SPを" + m_useSP * 100 + "消費した");
+    }
+}
---
+++ Assets/junjun/Scripts/UI/UIManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Scene上のUIを
+/// </summary>
+public class UIManager : SingletonMonoBehavior<UIManager>
+{
+    [SerializeField] ImgsFillDynamic m_playerHP;
+    [SerializeField] ImgsFillDynamic m_playerSP;
+    [SerializeField] float m_healValue;
+    public float span = 5f;
+
+
+    private float m_maxSP;
+    private float m_currentSP;
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        m_maxSP = m_playerSP.TargetValue;
+        m_currentSP = m_playerSP.TargetValue;
+        StartCoroutine("Logging");
+    }
+
+    IEnumerator Logging()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(span);
+            Debug.LogFormat("{0}秒経過", span);
+            NaturalRecoverySP();
+        }
+    }
+    /// <summary>
+    /// ダメージを受けた時によばれる関数
+    /// </summary>
+    /// <param name="damage"></param>
+    public void DecreasesHP(float damage)
+    {
+        m_playerHP.SetValue(damage, false, Random.Range(0.01f, 0.5f));
+    }
+
+    public void NaturalRecoverySP()
+    {
+        if (!(m_currentSP == m_maxSP))
+        {
+            m_playerSP.SetValue(m_healValue, false, Random.Range(0.01f, 0.5f));
+        }
+    }
+
+    /// <summary>
+    /// SPを消費した時に呼ばれる関数
+    /// </summary>
+    /// <param name="useSP"></param>
+    public void UseSP(float useSP)
+    {
+        m_playerSP.SetValue(useSP, false, Random.Range(0.01f, 0.5f));
+        m_currentSP -= useSP;
+    }
+}
